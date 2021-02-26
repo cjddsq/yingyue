7373171f31b177c01686a99898a580c6a9ab71a5
cjddsq <52540335+cjddsq@users.noreply.github.com>
2021-02-26 09:02:35 +0800

    Add files via upload

---
+++ Component/NavHeader/NavHeader.js
@@ -0,0 +1,30 @@
+// Component/NavHeader/NavHeader.js
+Component({
+  /**
+   * 组件的属性列表，由外部传入。
+   */
+  properties: {
+    title: {
+      type: String,
+      value: '我是titile默认值'
+    },
+    nav: {
+      type: String,
+      value: '我是nav默认值'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
